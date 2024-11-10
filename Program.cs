d8dde3970d2f9c96380487f501fa737ec96b89dc
Mityagin Roman <mityagin86@gmail.com>
2024-11-11 00:55:15 +0700

    Add first task

--- Program.cs
+++ Program.cs
@@ -1,6 +1,30 @@
-﻿
-// ------ Задача #1 -------
-string input = 
-decimal qwe(decimal a) {
+﻿// ------ Задача #1 -------
 
+bool isCorrect = false;
+
+while (isCorrect == false)
+{
+    Console.Write("Введите сумму дохода: ");
+    string? input = Console.ReadLine();
+
+    decimal summ = decimal.TryParse(input, out decimal parseInput) ? parseInput : 0;
+    if (summ <= 0) {
+        Console.WriteLine("Ошибка! Введите корректные данные!");
+        Console.WriteLine();
+    }
+    else {
+        taxCalculation(summ);
+        isCorrect = true;
+    }
 }
+
+void taxCalculation(decimal summ) {
+
+    decimal tax = Math.Round(summ * 13 / 100, 2);
+
+    Console.WriteLine($"Сумма налогов к уплате: {tax}");
+}
+
+
+
+
