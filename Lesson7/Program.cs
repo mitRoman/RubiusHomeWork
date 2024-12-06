1e885e5b4673e52f2652ebdf8f42f3e220632cd5
Mityagin Roman <67629750+mitRoman@users.noreply.github.com>
2024-12-06 16:46:09 +0700

    Add files via upload

---
+++ Lesson7/Program.cs
@@ -0,0 +1,42 @@
+﻿class Program
+{
+    public static async Task Main(string[] args)
+    {
+        string path = "resource\\Book.txt";
+
+        string fullPath = Path.Combine(Directory.GetCurrentDirectory(), path);
+
+        if (File.Exists(fullPath))
+        {
+            try
+            {
+                string content = await File.ReadAllTextAsync(fullPath);
+                int wordCount = 0;
+
+                wordCount = Counter.CountWords(content);
+
+                Console.WriteLine($"Количество слов (чтение всего файла): {wordCount}");
+
+                using (StreamReader reader = new StreamReader(fullPath))
+                {
+                    wordCount = 0;
+                    string? line;
+                    while ((line = await reader.ReadLineAsync()) != null)
+                    {
+                        wordCount += Counter.CountWords(line);
+                    }
+                }
+
+                Console.WriteLine($"Количество слов (потоковая обработка): {wordCount}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Файл не найден.");
+        }
+    }
+}
